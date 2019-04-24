fc13717bc11b953e90de74dfb12b88c66bdd75d9
olexiiy <49442492+olexiiy@users.noreply.github.com>
2019-04-24 15:23:46 +0300

    Update 61. Структуры t1.cpp

--- 61. Структуры t1.cpp
+++ 61. Структуры t1.cpp
@@ -2,41 +2,40 @@
 
 #include <iostream>
 
-// Сначала объявляем структуру Advertising 
 struct Advertising
 {
-	int adsShown;
-	double clickThroughRatePercentage;
-	double averageEarningsPerClick;
+	double viewed;
+	double clicked;
+	double paid;
 };
 
-void printAdvertising(Advertising ad)
+void outPut(Advertising mySite)
 {
 	using namespace std;
-	cout << "Number of ads shown: " << ad.adsShown << endl;
-	cout << "Click through rate: " << ad.clickThroughRatePercentage << endl;
-	cout << "Average earnings per click: $" << ad.averageEarningsPerClick << endl;
+	cout << "Number of ads shown: " << mySite.viewed << endl;
+	cout << "Click through rate: " << mySite.clicked << endl;
+	cout << "Average earnings per click: $" << mySite.paid << endl;
 
 	// Следующая строка кода разбита из-за своей длины
-	// Мы делим ad.clickThroughRatePercentage на 100, так как пользователь указывает проценты, а не готовое число
+	// Мы делим mySite.clicked на 100, так как пользователь указывает проценты, а не готовое число
 	cout << "Total Earnings: $" <<
-		(ad.adsShown * ad.clickThroughRatePercentage / 100 * ad.averageEarningsPerClick) << endl;
+		(mySite.viewed * mySite.clicked / 100 * mySite.paid) << endl;
+	
 }
+
 
 int main()
 {
 	using namespace std;
-	// Объявляем переменную структуры Advertising 
-	Advertising ad;
-
-	cout << "How many ads were shown today? ";
-	cin >> ad.adsShown;
-	cout << "What percentage of users clicked on the ads? ";
-	cin >> ad.clickThroughRatePercentage;
-	cout << "What was the average earnings per click? ";
-	cin >> ad.averageEarningsPerClick;
-
-	printAdvertising(ad);
+	Advertising mySite;
+    std::cout << "Input the Views: \n"; 
+	std::cin >> mySite.viewed;
+	std::cout << "Input the Clicked percent : \n";
+	std::cin >> mySite.clicked;
+	std::cout << "Input the Paid: \n";
+	std::cin >> mySite.paid;
+	outPut(mySite);
 
 	return 0;
+
 }
