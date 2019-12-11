211faf85aeebb00feaee2c2a51be1cbc159b72f3
olexiiy <49442492+olexiiy@users.noreply.github.com>
2019-12-11 21:57:08 +0200

    Update падение мачика.cpp

--- падение мачика.cpp
+++ падение мачика.cpp
@@ -6,34 +6,97 @@
 */
 
 
+/*
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace myConstants
+{
+    const double gravity(9.8);
+}
+#endif
+*/
 
 #include <iostream>
+#include "constants.h"
 
+int getValue()
+{
+    while (true) // цикл продолжается до тех пор, пока пользователь не введёт корректное значение
+    {
+       
+        int a;
+        std::cin >> a;
+
+        // Проверка на предыдущее извлечение
+        if (std::cin.fail()) // если предыдущее извлечение оказалось неудачным,
+        {
+            std::cin.clear(); // то возвращаем cin в 'обычный' режим работы
+            std::cin.ignore(32767, '\n'); // и удаляем значения предыдущего ввода из входного буфера
+            std::cout << "Oops, that input is invalid.  Please try again.\n";
+        }
+        else
+        {
+            std::cin.ignore(32767, '\n'); // удаляем лишние значения
+
+            return a;
+        }
+    }
+}
+
+
+// Получаем начальную высоту от пользователя и возвращаем её
+int getInitialHeight()
+{
+    std::cout << "Enter the height of the tower in meters: ";
+    int initialHeight{ getValue() };
+    return initialHeight;
+}
+
+// Возвращаем расстояние от земли после "..." секунд падения
+double calculateHeight(double initialHeight, int seconds)
+{
+    // Используем формулу: [ s = u * t + (a * t^2) / 2 ], где u(начальная скорость) = 0
+    double distanceFallen = (myConstants::gravity * seconds * seconds) / 2;
+    double currentHeight = initialHeight - distanceFallen;
+
+    return currentHeight;
+}
+
+// Выводим высоту, на которой находится мячик после каждой секунды падения
+void printHeight(const double& height, const int& seconds)
+{
+    if (height > 0.0)
+    {
+        std::cout << "At " << seconds << " seconds, the ball is at height:\t" << height <<
+            " meters\n";
+    }
+    else
+        std::cout << "At " << seconds << " seconds, the ball is on the ground.\n";
+}
+
+void calculateAndPrintHeight(const double& initialHeight, const int& seconds)
+{
+    double height = calculateHeight(initialHeight, seconds);
+    printHeight(height, seconds);
+}
 
 int main()
 {
-	std::cout << "Enter the height of the tower in meters: ";
-	int height{};
-	std::cin >> height;
-	double distanceFallen{};
-	for (int count{}; (static_cast<double>(height) - distanceFallen) > 0; count++)
-	{
-		distanceFallen = 9.8 * count * count / 2;
-		double currentHeight = height - distanceFallen;
-		if ((static_cast<double>(height) - distanceFallen) > 0)
-		{
-			std::cout << "At " << count << " seconds, the ball is at height:\t" << currentHeight <<
-				" meters\n";
-		}
-		else
-			std::cout << "At " << count << " seconds, the ball is on the ground.\n";
-	}
+    const int height{ getInitialHeight() };
+    
+    double distanceFallen{};
+    for (int seconds{}; (static_cast<double>(height) - distanceFallen) > 0; seconds++)
+    {
+        distanceFallen = myConstants::gravity * seconds * seconds / 2;
+        double currentHeight = height - distanceFallen;
+        printHeight(currentHeight, seconds);
+    }
 
-	double times = sqrt((2 * static_cast<double>(height)) / 9.8);
+    double times = sqrt((2 * static_cast<double>(height) / myConstants::gravity));
 
-	std::cout << "the ball need " << times << " seconds, to fall on the ground.\n";
+    std::cout << "the ball need " << times << " seconds, to fall on the ground.\n";
 
-	system("pause");
-
-	return 0;
+    return 0;
 }
+
