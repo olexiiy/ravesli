608594263b9692273e1ca521c10ffefdc76cdaad
olexiiy <49442492+olexiiy@users.noreply.github.com>
2020-04-27 17:54:08 +0300

    Update t5.2.cpp

--- t5.2.cpp
+++ t5.2.cpp
@@ -7,130 +7,112 @@
 программа должна спросить у пользователя, не хочет ли он поиграть ещё раз. Если пользователь не ведёт ни y, ни n (а
 что-то другое), то программа должна спросить его ещё раз.
 */
-
+#include <ctime> // раскомментируйте, если используете Code::Blocks
 #include <iostream>
- #include <ctime> // раскомментируйте, если используете Code::Blocks
 #include <random> // для std::random_device и std::mt19937
 
 int getRandomNumber(long min, long max)
 {
-	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
-	// Равномерно распределяем рандомное число в нашем диапазоне
-		
-	int randomNumber = static_cast<int>(rand() * fraction * (max - min + 1) + min); // генерируем случайное число в заданом диапазоне
-	return randomNumber; // возвращаем случайное число в заданом диапазоне 
-}
+    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+    // Равномерно распределяем рандомное число в нашем диапазоне
 
+    int randomNumber = static_cast<int>(rand() * fraction * (max - min + 1) + min); // генерируем случайное число в заданом диапазоне
+    return randomNumber; // возвращаем случайное число в заданом диапазоне
+}
 
 int getInteger()
 {
 
-	while (true) // цикл продолжается до тех пор, пока пользователь не введёт корректное значение
-	{
-		int x;
-		std::cin >> x;
+    while (true) // цикл продолжается до тех пор, пока пользователь не введёт корректное значение
+    {
+        int x;
+        std::cin >> x;
 
-		// Проверка на предыдущее извлечение
-		if (std::cin.fail()) // если предыдущее извлечение оказалось неудачным,
-		{
-			std::cin.clear(); // то возвращаем cin в 'обычный' режим работы
-			std::cin.ignore(32767, '\n'); // и удаляем значения предыдущего ввода из входного буфера
-			std::cout << "Oops, that input is invalid.  Please try again.\n";
-		}
-		else
-		{
-			std::cin.ignore(32767, '\n'); // удаляем лишние значения
+        // Проверка на предыдущее извлечение
+        if (std::cin.fail()) // если предыдущее извлечение оказалось неудачным,
+        {
+            std::cin.clear(); // то возвращаем cin в 'обычный' режим работы
+            std::cin.ignore(32767, '\n'); // и удаляем значения предыдущего ввода из входного буфера
+            std::cout << "Oops, that input is invalid.  Please try again.\n";
+        } else {
+            std::cin.ignore(32767, '\n'); // удаляем лишние значения
 
-			return x;
-		}
-	}
+            return x;
+        }
+    }
 }
 
 bool playAgain()
 {
-	while (true) // цикл продолжается до тех пор, пока пользователь не введёт корректное значение
-	{
-		std::cout << "Would you like to play again (y/n)?";
-		char answer;
-		std::cin >> answer;
+    while (true) // цикл продолжается до тех пор, пока пользователь не введёт корректное значение
+    {
+        std::cout << "Would you like to play again (y/n)?";
+        char answer;
+        std::cin >> answer;
 
-		// Переменные типа char могут принимать любые символы из пользовательского ввода, поэтому нам не стоит беспокоиться по поводу возникновения неудачного извлечения
+        // Переменные типа char могут принимать любые символы из пользовательского ввода, поэтому нам не стоит беспокоиться по поводу возникновения неудачного извлечения
 
-		std::cin.ignore(32767, '\n'); // удаляем лишний балласт
+        std::cin.ignore(32767, '\n'); // удаляем лишний балласт
 
-		// Выполняем проверку пользовательского ввода
-		if (answer == 'y' )
-			return true; // возвращаем обратно в caller
-		else if (answer == 'n')
-			return false; // возвращаем обратно в caller
-		else // в противном случае, сообщаем пользователю что что-то пошло не так
-			std::cout << "Oops, that input is invalid.  Please try again.\n";
-	}
+        // Выполняем проверку пользовательского ввода
+        if (answer == 'y')
+            return true; // возвращаем обратно в caller
+        else if (answer == 'n')
+            return false; // возвращаем обратно в caller
+        else // в противном случае, сообщаем пользователю что что-то пошло не так
+            std::cout << "Oops, that input is invalid.  Please try again.\n";
+    }
 }
 
 bool theGame(int randomNumber)
 {
-	for (int count = 1; count <= 7; ++count)
-	{
-		std::cout << "Guess #" << count << ": ";
-		int possible = getInteger();
-		if (possible == randomNumber)
-		{
+    for (int count = 1; count <= 7; ++count) {
+        std::cout << "Guess #" << count << ": ";
+        int possible = getInteger();
+        if (possible == randomNumber) {
 
-			return true;
-			
-		}
-		else if (possible > randomNumber)
-		{
-			std::cout << "Your guess is too high." << std::endl;
+            return true;
 
-			if (count == 7)
-			{
-				std::cout << "You lose";
-				return false;
-			}
-		}
-		else if (possible < randomNumber)
-		{
-			std::cout << "Your guess is too low." << std::endl;
-			if (count == 7)
-				{
-				std::cout << "You lose" << std::endl;
-				return false;
-		}
-		};
-	}
+        } else if (possible > randomNumber) {
+            std::cout << "Your guess is too high." << std::endl;
 
-
+            if (count == 7) {
+                std::cout << "You lose";
+                return false;
+            }
+        } else if (possible < randomNumber) {
+            std::cout << "Your guess is too low." << std::endl;
+            if (count == 7) {
+                std::cout << "You lose" << std::endl;
+                return false;
+            }
+        };
+    }
 }
 
 int main()
 {
-	std::mt19937 mersenne(static_cast<unsigned int>(time(0))); // инициализируем Вихрь Мерсенна стартовым числом основаным не времени
-	srand(static_cast<int>(mersenne()));  // в качестве стартового числа используем Вихрь Мерсенна случайным стартовым числом//
-	rand();
-	
-	
-	int randomNumber = getRandomNumber(0, 100);
-	
-	
-	int possible{};
-	do
-	{
-		int randomNumber = getRandomNumber(0, 100);
-		std::cout << randomNumber << std::endl;
-		std::cout << "Let's play a game. I'm thinking of a number. You have 7 tries to guess what it is." << std::endl;
-		bool won = theGame(randomNumber);
-		if (won == true)
-			std::cout << "Correct! You win!" << std::endl;
-		else
-		
-			std::cout << "Sorry, you lose. The correct number was " << randomNumber << std::endl;
- 	
-	}while (playAgain());
-	
-	std::cout << "thank you for the game";
+    std::mt19937 mersenne(static_cast<unsigned int>(time(0))); // инициализируем Вихрь Мерсенна стартовым числом основаным не времени
+    srand(static_cast<int>(mersenne())); // в качестве стартового числа используем Вихрь Мерсенна случайным стартовым числом//
+    rand();
 
+    int randomNumber = getRandomNumber(1, 111);
 
-	return 0;
+    do {
+
+        //        std::cout << randomNumber << std::endl;
+        std::cout << "Let's play a game. I'm thinking of a number. You have 7 tries to guess what it is." << std::endl;
+        bool won = theGame(randomNumber);
+        if (won == true)
+            std::cout << "Correct! You win!" << std::endl;
+        else
+
+            std::cout << "Sorry, you lose. The correct number was " << randomNumber << std::endl;
+
+    } while (playAgain());
+
+    std::cout << "thank you for the game";
+
+    return 0;
 }
+
