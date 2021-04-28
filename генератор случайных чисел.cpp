ce45362cbd98576d8115adcf25437f2bfa4b2eb6
olexiiy <49442492+olexiiy@users.noreply.github.com>
2021-04-28 15:53:04 +0300

    Update генератор случайных чисел.cpp

--- генератор случайных чисел.cpp
+++ генератор случайных чисел.cpp
@@ -2,25 +2,24 @@
  #include <ctime> // раскомментируйте, если используете Code::Blocks
 #include <random> // для std::random_device и std::mt19937
 
-int getRandomNumber(long min, long max) {
-  static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
-  // Равномерно распределяем рандомное число в нашем диапазоне
+int getRandomNumber(int min, int max) {
 
-  int randomNumber =
-      static_cast<int>(rand() * fraction * (max - min + 1) +
-                       min); // генерируем случайное число в заданом диапазоне
-  return randomNumber; // возвращаем случайное число в заданом диапазоне
+  std::random_device device; // для получения зерна лучше чем от time(NULL)
+  std::mt19937 mersenne(device()); // инициализируем Вихрь Мерсенна стартовым
+                                   // числом
+
+  std::uniform_int_distribution<> distribution(
+      min, max); // Равномерное распределение [10, 20]
+
+  int randomNumber = distribution(mersenne);
+  // генерируем случайное число в заданом диапазоне
+
+  return randomNumber;
+  // возвращаем случайное число в заданом диапазоне
 }
 
 int main() {
   setlocale(LC_ALL, "");
-
-  std::random_device device; // для получения зерна лучше чем от time(NULL)
-  std::mt19937 mersenne(device()); // инициализируем Вихрь Мерсенна стартовым
-                                   // числом основаным на времени
-  //      srand(static_cast<int>(mersenne()));  // в качестве стартового числа
-  //      используем Вихрь Мерсенна случайным стартовым числом// rand();  //
-  //      сбрасываем первый результат
 
   for (int count = 0; count < 200; ++count) {
     std::cout << getRandomNumber(-50000, 50000) << "\t";
